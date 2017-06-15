08db7b97bbe2c00d9f7966eaf001072121908291
ashuerta <ashuerta@hotmail.com>
2017-06-14 22:50:03 -0500

    ASHM

---
+++ Ashm/ASHM.Common.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASHM.Common.Entities
+{
+    public class Class1
+    {
+    }
+}
